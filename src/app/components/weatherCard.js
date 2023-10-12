ad673f40eedc60b6b4e49c7502acc8ace92302a9
Suedey <Ot2017@nyu.edu>
2023-10-12 09:30:39 -0400

    Push 3

--- src/app/components/weatherCard.js
+++ src/app/components/weatherCard.js
@@ -1,5 +1,6 @@
 "use-client";
 import styles from "./WeatherCard.module.css";
+import WeatherIcon from "./weathericon";
 
 const WeatherCard = ({
     cityName,
@@ -12,14 +13,15 @@ const WeatherCard = ({
     windSpeed,
 }) => (
     <div className={styles.weatherCardWrapper}>
+        <WeatherIcon weatherType={weatherType}/>
         <h2>{cityName}</h2>
         <p>Weather Type: {weatherType}</p>
-        <p>Current Temperature: {currentTemperature}</p>
-        <p>High Temperature: {highTemperature}</p>
-        <p>Low Temperature: {lowTemperature}</p>
-        <p>Cloudiness: {cloudiness}</p>
-        <p>Humidity: {humidity}</p>
-        <p>Wind Speed: {windSpeed}</p>
+        <p>Current Temperature: {currentTemperature}°F</p>
+        <p>High Temperature: {highTemperature}°F</p>
+        <p>Low Temperature: {lowTemperature}°F</p>
+        <p>Cloudiness: {cloudiness}%</p>
+        <p>Humidity: {humidity}%</p>
+        <p>Wind Speed: {windSpeed}mph</p>
         
     </div>
 
